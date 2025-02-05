c6478d38d107a9ef8922d18f153ee68c0cf21886
ADELL\adamk <adam.krupczynski@gmail.com>
2025-02-05 13:29:42 +0100

    BD-5882 React-native 0.76.6 update.

--- android/app/src/main/java/com/rnpointsdkminimalintegration/MainApplication.java
+++ android/app/src/main/java/com/rnpointsdkminimalintegration/MainApplication.java
@@ -1,16 +1,20 @@
 package com.rnpointsdkminimalintegration;
+
 import android.content.res.Configuration;
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
-
 import android.app.Application;
+import androidx.annotation.NonNull;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
 import com.facebook.soloader.SoLoader;
+import java.io.IOException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -48,6 +52,12 @@ public class MainApplication extends Application implements ReactApplication {
       });
 
   @Override
+  public ReactHost getReactHost() {
+      return ReactNativeHostWrapper.createReactHost(this, mReactNativeHost);
+  }
+
+  @NonNull
+  @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
@@ -55,7 +65,12 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+
+    try {
+        SoLoader.init(this, OpenSourceMergedSoMapping.INSTANCE);
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       DefaultNewArchitectureEntryPoint.load();
@@ -64,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   @Override
-  public void onConfigurationChanged(Configuration newConfig) {
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
     super.onConfigurationChanged(newConfig);
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
