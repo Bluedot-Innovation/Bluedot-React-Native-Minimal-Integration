1dca501364a0e044ba0000dbb0d0f54a1be397da
AdamK <adamkrupczynski@rezolve.com>
2026-05-06 15:51:04 +0200

    Showcase requirements for custom implementation of FirebaseMessagingService.

---
+++ android/app/src/pushDefault/java/com/rnpointsdkminimalintegration/MainApplication.java
@@ -0,0 +1,76 @@
+package com.rnpointsdkminimalintegration;
+
+import static com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative;
+import static com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost;
+
+import android.app.Application;
+import android.content.res.Configuration;
+import androidx.annotation.NonNull;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.soloader.OpenSourceMergedSoMapping;
+import com.facebook.soloader.SoLoader;
+import expo.modules.ApplicationLifecycleDispatcher;
+import io.bluedot.pushnotifications.PushNotificationsSdkPackage;
+import java.io.IOException;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new DefaultReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          packages.add(new PushNotificationsSdkPackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+      };
+
+  @Override
+  public ReactHost getReactHost() {
+    return getDefaultReactHost(this, mReactNativeHost, null);
+  }
+
+  @NonNull
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    loadReactNative(this);
+    ApplicationLifecycleDispatcher.onApplicationCreate(this);
+  }
+
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
+  }
+}
